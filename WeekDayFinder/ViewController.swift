e47b0acd1898cf503f345c608199c8bc369ed91b
fairmentat <fairmentat@mail.ru>
2020-03-25 01:23:18 +0400

    End UI

--- WeekDayFinder/ViewController.swift
+++ WeekDayFinder/ViewController.swift
@@ -15,6 +15,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var dateTF: UITextField!
+    
+    
+    @IBOutlet weak var monthTF: UITextField!
+    
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay() {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
